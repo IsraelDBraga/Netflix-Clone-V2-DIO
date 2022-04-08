2b90c4861b408eaf466619bc68db14d4372e3a0c
IsraelDBraga <israeldbraga1@gmail.com>
2022-04-08 13:34:07 -0300

    projeto netflix clone

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+      // toggle mobile menu
+    $('[data-toggle="toggle-nav"]').on('click', function () {
+        $(this).closest('nav').find($(this).attr('data-target')).toggleClass('hidden');
+        return false;
+    });
+
+    // feather icons
+    feather.replace();
+
+
+
+});
